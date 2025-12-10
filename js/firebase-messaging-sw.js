c8d47e207446e9f91a94f6c30f28a43352f76cf3
daisuke orita <bestbiribox5421@gmail.com>
2025-12-10 11:22:20 +0700

    アプデ

--- js/firebase-messaging-sw.js
+++ js/firebase-messaging-sw.js
@@ -24,7 +24,7 @@ messaging.onBackgroundMessage((payload) => {
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: '/icon.png' // アイコン画像があればそのパス、なければ省略可
+    icon: '/icon.png' // アイコン画像がなければデフォルトのが表示されます
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
